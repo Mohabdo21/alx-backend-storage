0c25ed916382907bd139fd8def37b330748f2d6e
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-13 02:18:26 +0400

    just copy and try

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,28 +2,16 @@
 -- computes and stores the average score for a student
 -- This script is part of the Back-end SQL MySQL curriculum
 
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-	DECLARE avg_score DECIMAL(5,2);
-
-	-- compute the average score
-	SELECT AVG(CAST(score AS DECIMAL)) INTO avg_score
-	FROM corrections
-	WHERE user_id = user_id;
-
-	-- store the average score in the `users` table
 	UPDATE users
-	SET average_score = avg_score
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
 	WHERE id = user_id;
 
-	-- Compute the average and store it into the `users` table directly
-	-- UPDATE users
-	-- SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
-	-- WHERE id = user_id;
 END $$
 
 DELIMITER ;
