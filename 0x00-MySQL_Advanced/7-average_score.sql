99faedab237d3d80b0dea6135308b527ebfe9b21
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-13 01:52:13 +0400

    calculate the average directly

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,12 +6,17 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE avg_score FLOAT;
+	-- DECLARE avg_score FLOAT;
 
 	-- compute the average score
-	SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+	-- SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
 
 	-- store the average score in the `users` table
-	UPDATE users SET average_score = avg_score WHERE id = user_id;
+	-- UPDATE users SET average_score = avg_score WHERE id = user_id;
+
+	-- Compute the average and store it into the `users` table directly
+	UPDATE users
+	SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
+	WHERE id = user_id;
 END;$$
 DELIMITER ;
