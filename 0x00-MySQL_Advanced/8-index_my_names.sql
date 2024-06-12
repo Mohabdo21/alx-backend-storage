b73583c5cdc30480a2ffdfa4b328ff764e069fc9
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-13 02:51:04 +0400

    another approach

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -2,7 +2,7 @@
 -- This script is part of the Back-end SQL MySQL curriculum
 
 -- Add a generated column for the first letter of the name
-ALTER TABLE names ADD COLUMN name_first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1));
+-- ALTER TABLE names ADD COLUMN name_first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1));
 
 -- Create an index on the generated column
-CREATE INDEX idx_name_first ON names(name_first_letter);
+CREATE INDEX idx_name_first ON names(name(1));
