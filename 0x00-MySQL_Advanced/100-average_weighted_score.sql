e40a3ad896f012cc5d11d0432470e191307c17c8
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-13 03:35:33 +0400

    missing semi

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -10,10 +10,10 @@ BEGIN
 	DECLARE avg_weighted_score FLOAT;
 
 	-- compute the average weighted score
-	SELECT SUM(c.score * p.weight) / SUM(p.weight) INTO avg_weighted_score
-	FROM corrections c 
-	JOIN projects p ON c.project_id = p.id
-	WHERE c.user_id = user_id
+    SELECT SUM(c.score * p.weight) / SUM(p.weight) INTO avg_weighted_score
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    WHERE c.user_id = user_id;
 
 	-- store the average score in the `users` table
 	UPDATE users SET average_score = avg_weighted_score WHERE id = user_id;
