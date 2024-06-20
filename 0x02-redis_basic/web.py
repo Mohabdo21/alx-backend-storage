be98ad52bcce87f406788cb9eef9ebacc3ab81cc
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-20 06:42:41 +0400

    rollback

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,25 +1,67 @@
 #!/usr/bin/env python3
 """
-Module for implementing an expiring web cache and tracker
-to obtain the HTML content of a particular URL and return it.
+Module for web
 """
+from functools import wraps
+from typing import Callable
+
 import redis
 import requests
 
-r = redis.Redis()
-count = 0
+# Initialize Redis client outside to reuse the connection
+_redis = redis.Redis()
 
 
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator to count the number of calls to a method
+    """
+
+    @wraps(method)
+    def wrapper(url: str, *args, **kwargs):
+        """
+        Wrapper function for the decorator
+        """
+        _redis.incr(f"count:{url}")
+        return method(url, *args, **kwargs)
+
+    return wrapper
+
+
+def cache_result(expiration: int = 10) -> Callable:
+    """
+    Decorator to cache the result of a function call with an expiration time
+    """
+
+    def decorator(method: Callable) -> Callable:
+        @wraps(method)
+        def wrapper(url: str, *args, **kwargs):
+            """
+            Wrapper function for caching the result
+            """
+            cached_content = _redis.get(url)
+            if cached_content:
+                return cached_content.decode("utf-8")
+            result = method(url, *args, **kwargs)
+            _redis.setex(url, expiration, result)
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+@count_calls
+@cache_result(expiration=10)
 def get_page(url: str) -> str:
-    """track how many times a particular URL was accessed in the key
-    "count:{url}"
-    and cache the result with an expiration time of 10 seconds"""
-    r.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
-    return resp.text
+    """
+    Get the HTML content of a particular URL
+    """
+    response = requests.get(url)
+    # response.raise_for_status()  # Handle HTTP errors
+    return response.text
 
 
 if __name__ == "__main__":
-    get_page("http://slowwly.robertomurray.co.uk")
+    url = "http://slowwly.robertomurray.co.uk"
+    get_page(url)
