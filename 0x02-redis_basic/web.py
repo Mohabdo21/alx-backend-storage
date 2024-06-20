773bc8044f02136eef967308d1af90e01beb1386
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-20 06:40:50 +0400

    copy

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,44 +3,23 @@
 Module for implementing an expiring web cache and tracker
 to obtain the HTML content of a particular URL and return it.
 """
-from typing import Optional
-
 import redis
 import requests
 
-# Initialize the Redis client
 r = redis.Redis()
+count = 0
 
 
 def get_page(url: str) -> str:
-    """
-    Track how many times a particular URL was accessed
-    and cache the result with an expiration time of 10 seconds.
-
-    Args:
-        url (str): The URL to fetch.
-
-    Returns:
-        str: The HTML content of the URL.
-    """
-    try:
-        # Check if the URL is already cached
-        cached_content: Optional[bytes] = r.get(f"cached:{url}")
-        if cached_content:
-            return cached_content.decode("utf-8")
-
-        response = requests.get(url)
-        response.raise_for_status()
-
-        r.incr(f"count:{url}")
-
-        r.setex(f"cached:{url}", 10, response.text)
-        return response.text
-    except requests.RequestException as e:
-        print(f"Error fetching {url}: {e}")
-        return ""
+    """track how many times a particular URL was accessed in the key
+    "count:{url}"
+    and cache the result with an expiration time of 10 seconds"""
+    r.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    return resp.text
 
 
 if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    print(get_page(url))
+    get_page("http://slowwly.robertomurray.co.uk")
